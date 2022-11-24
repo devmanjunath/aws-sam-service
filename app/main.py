548063dc6ab3b01c21f9aa8fe618352a68ea5fee
manju-pc <manjunathpv@outlook.com>
2022-11-24 17:34:39 +0530

    initial commit

---
+++ app/main.py
@@ -0,0 +1,19 @@
+import logging
+from logging.config import fileConfig
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI()
+
+# fileConfig("logging.conf", disable_existing_loggers=False)
+
+logger = logging.getLogger(__name__)
+
+
+@app.get("/")
+def index():
+    logger.warning("logging from the root logger")
+    return {"message": "Welcome"}
+
+
+handler = Mangum(app)
